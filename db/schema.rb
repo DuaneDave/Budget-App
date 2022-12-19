f64655921fd6cab384b4102c12b2dbc6aa4c2366
Duane David <duanedave17@gmail.com>
2022-12-19 06:11:27 -0800

    Add devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,53 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_218_932_033) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_140332) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'entities', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'amount'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.integer "total_amount", default: 0
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
-  create_table 'groups', force: :cascade do |t|
-    t.string 'name'
-    t.string 'icon'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id', null: false
-    t.integer 'total_amount', default: 0
-    t.index ['user_id'], name: 'index_groups_on_user_id'
+  create_table "money_groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
+    t.bigint "operation_id", null: false
+    t.index ["group_id"], name: "index_money_groups_on_group_id"
+    t.index ["operation_id"], name: "index_money_groups_on_operation_id"
   end
 
-  create_table 'money_groups', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'group_id', null: false
-    t.bigint 'operation_id', null: false
-    t.index ['group_id'], name: 'index_money_groups_on_group_id'
-    t.index ['operation_id'], name: 'index_money_groups_on_operation_id'
+  create_table "operations", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_operations_on_user_id"
   end
 
-  create_table 'operations', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'amount'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id', null: false
-    t.index ['user_id'], name: 'index_operations_on_user_id'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
-
-  add_foreign_key 'groups', 'users'
-  add_foreign_key 'money_groups', 'groups'
-  add_foreign_key 'money_groups', 'operations'
-  add_foreign_key 'operations', 'users'
+  add_foreign_key "groups", "users"
+  add_foreign_key "money_groups", "groups"
+  add_foreign_key "money_groups", "operations"
+  add_foreign_key "operations", "users"
 end
