c79130626c82cdf86edc51dd7469833f6b09a725
Duane David <duanedave17@gmail.com>
2022-12-19 06:50:20 -0800

    New migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_140332) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_542631) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
@@ -52,6 +51,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_140332) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
