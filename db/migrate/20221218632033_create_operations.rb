c7010f4d2b9d57bd7012608b1865a3d7f3389291
Duane David <duanedave17@gmail.com>
2022-12-19 04:11:18 -0800

    Add migrations

---
+++ db/migrate/20221218632033_create_operations.rb
@@ -0,0 +1,9 @@
+class CreateOperations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :operations do |t|
+      t.string :name
+      t.integer :amount
+      t.timestamps
+    end
+  end
+end
