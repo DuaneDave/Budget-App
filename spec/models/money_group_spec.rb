34fae49e08057ae7cefac2e1acf89f80ff88e6c0
Duane David <duanedave17@gmail.com>
2022-12-21 06:29:11 -0800

    Update money test

--- spec/models/money_group_spec.rb
+++ spec/models/money_group_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MoneyGroup, type: :model do
-  let(:user) { User.create(name: 'Rida Arif', email: 'example@mail.com', password: 'password') }
+  let(:user) { User.create(name: 'dee', email: 'duanedave@gmail.com', password: 'testing') }
   let(:group) { Group.create(name: 'Shopping', icon: 'https://i.imgur.com/Ar3Lf3Dt.png', user_id: user.id) }
 
   describe 'Validations' do
@@ -9,17 +9,17 @@ RSpec.describe MoneyGroup, type: :model do
       it { expect(group).to be_valid }
     end
 
-    it 'should allow valid name' do
+    it 'should allow a valid name' do
       group.name = 'Eg'
       expect(group).to_not be_valid
     end
 
-    it 'should allow valid name' do
+    it 'should allow a valid name' do
       group.name = 'Outing'
       expect(group).to be_valid
     end
 
-    it 'should allow valid user_id' do
+    it 'should allow valid a user_id' do
       group.user_id = nil
       expect(group).to_not be_valid
     end
